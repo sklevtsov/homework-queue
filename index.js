395faaf9dfb4d07505cab468fb5350d49993a84b
Kirill Molchanov <kirill-m@yandex-team.ru>
2016-06-16 15:11:04 +0300

    1.0.0 release

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+function LinkedList() {
+    this.length = 0;
+    this.head = null;
+}
+
+LinkedList.prototype.add = function(value) {
+    let node = new Node(value);
+    let currentNode = this.head;
+ 
+    if (!currentNode) {
+        this.head = node;
+        this.length++;
+         
+        return node;
+    }
+ 
+    while (currentNode.next) {
+        currentNode = currentNode.next;
+    }
+ 
+    currentNode.next = node;
+ 
+    this.length++;
+     
+    return node;
+};
+
+module.exports = LinkedList;
