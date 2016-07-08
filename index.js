dbf414985290f990011caa1c84be3640f48539af
Kirill Molchanov <kirill-m@yandex-team.ru>
2016-07-08 13:48:02 +0300

    add dequeue; add tests

--- index.js
+++ index.js
@@ -4,12 +4,35 @@ function Queue() {
 
     let queue = [];
 
-    this.enqueue = (item => {
-        queue.push(item);
+    this.enqueue = (value => {
+        queue.push(value);
     });
 
     this.isEmpty = (() => {
         return (queue.length === 0);
+    });
+
+    this.peek = (() => {
+        if (queue.length === 0) {
+            return 'queue is empty';
+        } else {
+            return queue[0];
+        }
+    });
+
+    this.dequeue = (() => {
+
+        if (queue.length === 0) {
+            return 'queue is empty';
+        } else {
+
+            let elem = queue[0];
+            queue = queue.slice(1);
+
+            return elem;
+
+        }
+
     });
 
 }
--- test/queue.js
+++ test/queue.js
@@ -9,20 +9,63 @@ describe('', () => {
 
     it('Queue should be a function', () => {
         console.log(myQueue);
+        console.log();
+
         expect(typeof Queue).to.equal('function');
     });
 
-    it('new Queue() should be an object', () => {
-        expect(typeof myQueue).to.equal('object');
+    describe('empty by default', () => {
+
+        it('queue should be empty by default', () => {
+            expect(myQueue.isEmpty()).to.be.true;
+        });
+
+        it('peek at empty queue should not return elements', () => {
+            expect(myQueue.peek()).to.equal('queue is empty');
+        });
+
+        it('dequeue at empty queue should not return elements', () => {
+            expect(myQueue.dequeue()).to.equal('queue is empty');
+        });
+ 
     });
 
-    it('queue should be empty by default', () => {
-        expect(myQueue.isEmpty()).to.be.true;
+    describe('elems added', () => {
+
+        it('queue should not be empty after elem add', () => {
+            myQueue.enqueue('first');
+            expect(myQueue.isEmpty()).to.be.false;
+        });
+
+        it('peek should return first elem', () => {
+            expect(myQueue.peek()).to.equal('first');
+        });
+
+        it('peek should return first elem after one more elem add', () => {
+            myQueue.enqueue('second');
+            expect(myQueue.peek()).to.equal('first');
+        });
+
     });
 
-    it('queue should not be empty after elem add', () => {
-        myQueue.enqueue('first');
-        expect(myQueue.isEmpty()).to.be.false;
+    describe('elems dequeue', () => {
+
+        it('dequeue should return first elem', () => {
+            expect(myQueue.dequeue()).to.equal('first');
+        });
+
+        it('after dequeue peek should return secondary added elem', () => {
+            expect(myQueue.peek()).to.equal('second');
+        });
+
+        it('dequeue should return elem even if it alone', () => {
+            expect(myQueue.dequeue()).to.equal('second');
+        });
+
+        it('after dequeue all elems queue should be empty', () => {
+            expect(myQueue.isEmpty()).to.be.true;
+        });
+
     });
 
 });
